16342d5b23fcbc5308ed1ef5fdd3022ac28c9259
YuryShageev <schageev.yur@yandex.ru>
2022-10-15 09:31:17 +0300

    Вторая часть про массивы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        generateRandomArray();
+    }
+
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        int maxPayment = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        //Вариант 1
+        for (int i = 0; i < arr.length; i++) {
+            maxPayment += arr[i];
+        }
+//Вариант 2
+//        for (int allPayment : arr) {
+//            maxPayment += allPayment;
+//        }
+        System.out.println("Сумма трат за месяц составила " + maxPayment + " рублей");
+
+        int maxPaymentPerDay = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxPaymentPerDay) {
+                maxPaymentPerDay = arr[i];
+            }
+        }
+        System.out.println(maxPaymentPerDay);
+
+        int minPaymentPerDay = 210000;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < minPaymentPerDay) {
+                minPaymentPerDay = arr[i];
+            }
+        }
+        System.out.println(minPaymentPerDay);
+        return arr;
     }
 }
